cd74d4eebcdcb9a55db1b44140b3cf263ab5c80d
FrechetteMathieu <frechette.mathieu@carrefour.cegepvicto.ca>
2016-11-14 10:41:48 -0500

    Sprite animé pour le héro
    Le héros se déplace avec un sprite animé et la détection des bordures de
    fenêtre fonctionne.

--- ExercicesJeux/Exercice01/Game1.cs
+++ ExercicesJeux/Exercice01/Game1.cs
@@ -11,6 +11,12 @@ namespace Exercice01
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        KeyboardState keys = new KeyboardState();
+        KeyboardState previousKeys = new KeyboardState();
+        GameObjectAnime link;
+        Rectangle fenetre;
+        GameObject heros;
+        GameObject ennemis;
 
         public Game1()
         {
@@ -27,6 +33,10 @@ namespace Exercice01
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
+            this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
+            //this.graphics.ApplyChanges();
+            this.graphics.ToggleFullScreen(); // Fullscreen
 
             base.Initialize();
         }
@@ -41,6 +51,29 @@ namespace Exercice01
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            fenetre = graphics.GraphicsDevice.Viewport.Bounds;
+            fenetre.Width = graphics.GraphicsDevice.DisplayMode.Width;
+            fenetre.Height = graphics.GraphicsDevice.DisplayMode.Height;
+
+            link = new GameObjectAnime();
+            link.position = new Rectangle(300,100,75,75);
+            link.vitesse = 2;
+            link.sprite = Content.Load<Texture2D>("LinkSheet.png");
+            link.direction = Vector2.Zero;
+            link.etat = GameObjectAnime.Etat.AttenteDroite;
+
+            //heros = new GameObject();
+            //heros.estVivant = true;
+            //heros.vitesse = 5;
+            //heros.sprite = Content.Load<Texture2D>("LinkSheet.png");
+            //heros.direction = GameObject.directionObjet.droite;
+            //heros.position = new Rectangle(100,100,75,75);
+
+            ennemis = new GameObject();
+            ennemis.estVivant = true;
+            ennemis.vitesse = 3;
+            ennemis.sprite = Content.Load<Texture2D>("Ennemis01.png");
+            ennemis.position = new Rectangle(fenetre.Right - ennemis.sprite.Width, fenetre.Bottom/2, ennemis.sprite.Width, ennemis.sprite.Height);
         }
 
         /// <summary>
@@ -63,8 +96,123 @@ namespace Exercice01
                 Exit();
 
             // TODO: Add your update logic here
+            //switch (Keyboard.GetState().GetPressedKeys)
+            //{
+
+            //}
+
+            
+
+            //if (Keyboard.GetState().IsKeyDown(Keys.D))
+            //{
+            //    heros.direction = GameObject.directionObjet.droite;
+            //    heros.position.X += heros.vitesse;
+            //}
+            //else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            //{
+            //    heros.direction = GameObject.directionObjet.gauche;
+            //    heros.position.X -= heros.vitesse;
+            //}
+            //else if (Keyboard.GetState().IsKeyDown(Keys.W))
+            //{
+            //    heros.direction = GameObject.directionObjet.haut;
+            //    heros.position.Y -= heros.vitesse;
+            //}
+            //else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            //{
+            //    heros.direction = GameObject.directionObjet.bas;
+            //    heros.position.Y += heros.vitesse;
+            //}
+
+            UpdateHeros(gameTime);
 
             base.Update(gameTime);
+        }
+
+        protected void UpdateHeros(GameTime gameTime)
+        {
+
+            keys = Keyboard.GetState();
+            link.position.X += (int)(link.vitesse * link.direction.X);
+            link.position.Y += (int)(link.vitesse * link.direction.Y);
+
+            // Touche de direction droite
+            if (keys.IsKeyDown(Keys.Right))
+            {
+                link.direction.X = 2;
+                link.etat = GameObjectAnime.Etat.MarcheDroite;
+            }
+            if (keys.IsKeyUp(Keys.Right) && previousKeys.IsKeyDown(Keys.Right))
+            {
+                link.direction.X = 0;
+                link.etat = GameObjectAnime.Etat.AttenteDroite;
+            }
+
+            // Touche de direction gauche
+            if (keys.IsKeyDown(Keys.Left))
+            {
+                link.direction.X = -2;
+                link.etat = GameObjectAnime.Etat.MarcheGauche;
+            }
+            if (keys.IsKeyUp(Keys.Left) && previousKeys.IsKeyDown(Keys.Left))
+            {
+                link.direction.X = 0;
+                link.etat = GameObjectAnime.Etat.AttenteGauche;
+            }
+
+            // Touche de direction haut
+            if (keys.IsKeyDown(Keys.Up))
+            {
+                link.direction.Y = -2;
+                link.etat = GameObjectAnime.Etat.MarcheHaut;
+            }
+            if (keys.IsKeyUp(Keys.Up) && previousKeys.IsKeyDown(Keys.Up))
+            {
+                link.direction.Y = 0;
+                link.etat = GameObjectAnime.Etat.AttenteHaut;
+            }
+
+            // Touche de direction bas
+            if (keys.IsKeyDown(Keys.Down))
+            {
+                link.direction.Y = 2;
+                link.etat = GameObjectAnime.Etat.MarcheBas;
+            }
+            if (keys.IsKeyUp(Keys.Down) && previousKeys.IsKeyDown(Keys.Down))
+            {
+                link.direction.Y = 0;
+                link.etat = GameObjectAnime.Etat.AttenteBas;
+            }
+
+            // Touche attaque primaire
+            //if (keys.IsKeyDown(Keys.A))
+            //{
+            //    link.etat = GameObjectAnime.Etat.AttaqueDroite;
+            //}
+
+
+
+            // Teste les bordures d'écran
+            // A FAIRE: Trouver un façon d'avoir les dimensions de la sprite pour faire le teste des bordures
+            if (link.position.X < fenetre.Left)
+            {
+                link.position.X = fenetre.Left;
+            }
+            else if (link.position.X + 75 > fenetre.Right)
+            {
+                link.position.X = fenetre.Right-75;
+            }
+            else if(link.position.Y < fenetre.Top)
+            {
+                link.position.Y = fenetre.Top;
+            }
+            else if(link.position.Y + 74> fenetre.Bottom) // Le nombre est calculé selon la hauteur du sprite de marche
+            {
+                link.position.Y = fenetre.Bottom - 74;
+            }
+
+            link.Update(gameTime);
+            previousKeys = keys;
         }
 
         /// <summary>
@@ -76,6 +224,15 @@ namespace Exercice01
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            //spriteBatch.Draw(heros.sprite, heros.position, Color.White);
+            //spriteBatch.Draw(heros.sprite, heros.position, heros.rectSprite, Color.White);
+
+            spriteBatch.Draw(link.sprite, link.position, link.spriteAffiche, Color.White);
+
+            spriteBatch.Draw(ennemis.sprite, ennemis.position, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ ExercicesJeux/Exercice01/GameObjectAnime.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice01
+{
+    class GameObjectAnime
+    {
+        public Rectangle position;
+        public int vitesse;
+        public Texture2D sprite;
+        public bool estVivant;
+        public Vector2 direction;
+        public Rectangle spriteAffiche;
+
+        public enum Etat { MarcheDroite, AttenteDroite, MarcheGauche, AttenteGauche, MarcheHaut, AttenteHaut, MarcheBas, AttenteBas, AttaqueDroite };
+        public Etat etat;
+        private int compteur = 0;
+
+        // Gestion des tableaux de sprites (chaque sprite est un rectangle dans le tableau)
+
+        // Marche
+        int marcheState = 0; //État de départ
+        int nbEtatsMarche = 2; // Combien il y a de rectanbles pour l'état "Marcher"
+
+        public Rectangle[] tabMarcheDroite =
+        {
+            new Rectangle(421, 140, 71, 75),
+            new Rectangle(421, 0, 71, 70)
+        };
+
+        public Rectangle[] tabMarcheGauche =
+        {
+            new Rectangle(140, 0, 70, 74),
+            new Rectangle(145, 140, 70, 70)
+        };
+
+        public Rectangle[] tabMarcheHaut =
+        {
+            new Rectangle(290, 0, 70, 74),
+            new Rectangle(290, 140, 70, 75)
+        };
+
+        public Rectangle[] tabMarcheBas =
+        {
+            new Rectangle(0, 0, 70, 74),
+            new Rectangle(0, 140, 70, 75)
+        };
+
+
+        // Arrêt
+        int waitState = 0;
+        public Rectangle[] tabAttenteDroite = {new Rectangle(421, 140, 71, 75)};
+        public Rectangle[] tabAttenteGauche = {new Rectangle(140, 0, 70, 74)};
+        public Rectangle[] tabAttenteHaut = {new Rectangle(290, 0, 70, 74)};
+        public Rectangle[] tabAttenteBas = {new Rectangle(0, 0, 70, 74)};
+
+        // Attaque
+        int attackState = 0;
+        public Rectangle[] tabAttaqueDroite = {new Rectangle(393, 421, 75, 72)};
+
+
+        public virtual void Update(GameTime gameTime)
+        {
+            // Attente
+            if (etat == Etat.AttenteDroite)
+            {
+                spriteAffiche = tabAttenteDroite[waitState];
+            }
+            if (etat == Etat.AttenteGauche)
+            {
+                spriteAffiche = tabAttenteGauche[waitState];
+            }
+            if (etat == Etat.AttenteHaut)
+            {
+                spriteAffiche = tabAttenteHaut[waitState];
+            }
+            if (etat == Etat.AttenteBas)
+            {
+                spriteAffiche = tabAttenteBas[waitState];
+            }
+
+            // Marche
+            if (etat == Etat.MarcheDroite)
+            {
+                spriteAffiche = tabMarcheDroite[marcheState];
+            }
+            if (etat == Etat.MarcheGauche)
+            {
+                spriteAffiche = tabMarcheGauche[marcheState];
+            }
+            if (etat == Etat.MarcheHaut)
+            {
+                spriteAffiche = tabMarcheHaut[marcheState];
+            }
+            if (etat == Etat.MarcheBas)
+            {
+                spriteAffiche = tabMarcheBas[marcheState];
+            }
+
+            // Attaque
+            if(etat == Etat.AttaqueDroite)
+            {
+                spriteAffiche = tabAttaqueDroite[attackState];
+            }
+
+
+
+            // Compteur permettant de gérer le changement d'images
+            compteur++;
+            if (compteur == 8)
+            {
+                marcheState++;
+                if (marcheState == nbEtatsMarche)
+                {
+                    marcheState = 0;
+                }
+
+                compteur = 0;
+            }
+        }
+    }
+}
